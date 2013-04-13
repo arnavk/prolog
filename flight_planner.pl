ed887669d2af3f36a8123d7e5437508115cf0560
Arnav Kumar <arnavkumar24@hotmail.com>
2013-04-14 05:42:11 +0800

    Fully functional

--- flight_planner.pl
+++ flight_planner.pl
@@ -8,35 +8,130 @@ flight(ba24, london, singapore, 1000, 1600, wed).
 flight(ba24, london, singapore, 1000, 1600, thu).
 flight(ba24, london, singapore, 1000, 1600, sat).
 
-flight(ba4732, london, edinburgh, 0940, 1050, mon).
-flight(ba4732, london, edinburgh, 0940, 1050, tue).
-flight(ba4732, london, edinburgh, 0940, 1050, wed).
-flight(ba4732, london, edinburgh, 0940, 1050, thu).
-flight(ba4732, london, edinburgh, 0940, 1050, fri).
-flight(ba4732, london, edinburgh, 0940, 1050, sat).
-flight(ba4732, london, edinburgh, 0940, 1050, sun).
+flight(ba4732, london, edinburgh, 0940, 1050, mon).
+flight(ba4732, london, edinburgh, 0940, 1050, tue).
+flight(ba4732, london, edinburgh, 0940, 1050, wed).
+flight(ba4732, london, edinburgh, 0940, 1050, thu).
+flight(ba4732, london, edinburgh, 0940, 1050, fri).
+flight(ba4732, london, edinburgh, 0940, 1050, sat).
+flight(ba4732, london, edinburgh, 0940, 1050, sun).
 
-flight(ba4735, london, edinburgh, 1140, 1250, mon).
-flight(ba4735, london, edinburgh, 1140, 1250, tue).
-flight(ba4735, london, edinburgh, 1140, 1250, wed).
-flight(ba4735, london, edinburgh, 1140, 1250, thu).
-flight(ba4735, london, edinburgh, 1140, 1250, fri).
-flight(ba4735, london, edinburgh, 1140, 1250, sat).
-flight(ba4735, london, edinburgh, 1140, 1250, sun).
+flight(ba4735, london, edinburgh, 1140, 1250, mon).
+flight(ba4735, london, edinburgh, 1140, 1250, tue).
+flight(ba4735, london, edinburgh, 1140, 1250, wed).
+flight(ba4735, london, edinburgh, 1140, 1250, thu).
+flight(ba4735, london, edinburgh, 1140, 1250, fri).
+flight(ba4735, london, edinburgh, 1140, 1250, sat).
+flight(ba4735, london, edinburgh, 1140, 1250, sun).
 
-flight(ba4822, london, edinburgh, 1840, 1950, mon).
+flight(ba4822, london, edinburgh, 1840, 1950, mon).
 flight(ba4822, london, edinburgh, 1840, 1950, tue).
-flight(ba4822, london, edinburgh, 1840, 1950, wed).
-flight(ba4822, london, edinburgh, 1840, 1950, thu).
-flight(ba4822, london, edinburgh, 1840, 1950, fri).
+flight(ba4822, london, edinburgh, 1840, 1950, wed).
+flight(ba4822, london, edinburgh, 1840, 1950, thu).
+flight(ba4822, london, edinburgh, 1840, 1950, fri).
 
 flight(ba4733, edinburgh, london, 0830, 0940, mon).
-flight(ba4733, edinburgh, london, 0830, 0940, tue).
+flight(ba4733, edinburgh, london, 0830, 0940, tue).
 flight(ba4733, edinburgh, london, 0830, 0940, wed).
-flight(ba4733, edinburgh, london, 0830, 0940, thu).
+flight(ba4733, edinburgh, london, 0830, 0940, thu).
 flight(ba4733, edinburgh, london, 0830, 0940, fri).
-flight(ba4733, edinburgh, london, 0830, 0940, sat).
+flight(ba4733, edinburgh, london, 0830, 0940, sat).
 flight(ba4733, edinburgh, london, 0830, 0940, sun).
+
+flight(ba4736, edinburgh, london, 1340, 1450, mon). 
+flight(ba4736, edinburgh, london, 1340, 1450, tue).
+flight(ba4736, edinburgh, london, 1340, 1450, wed).
+flight(ba4736, edinburgh, london, 1340, 1450, thu).
+flight(ba4736, edinburgh, london, 1340, 1450, fri).
+flight(ba4736, edinburgh, london, 1340, 1450, sat).
+flight(ba4736, edinburgh, london, 1340, 1450, sun).
+
+flight(ba4833, edinburgh, london, 1940, 2050, mon).
+flight(ba4833, edinburgh, london, 1940, 2050, tue).
+flight(ba4833, edinburgh, london, 1940, 2050, wed).
+flight(ba4833, edinburgh, london, 1940, 2050, thu).
+flight(ba4833, edinburgh, london, 1940, 2050, fri).
+flight(ba4833, edinburgh, london, 1940, 2050, sun).
+
+flight(ba614, london, greece, 0910, 1245, mon).
+flight(ba614, london, greece, 0910, 1245, tue).
+flight(ba614, london, greece, 0910, 1245, wed).
+flight(ba614, london, greece, 0910, 1245, thu).
+flight(ba614, london, greece, 0910, 1245, fri).
+flight(ba614, london, greece, 0910, 1245, sat).
+flight(ba614, london, greece, 0910, 1245, sun).
+
+flight(sr805, london, greece, 1445, 1820, mon).
+flight(sr805, london, greece, 1445, 1820, tue).
+flight(sr805, london, greece, 1445, 1820, wed).
+flight(sr805, london, greece, 1445, 1820, thu).
+flight(sr805, london, greece, 1445, 1820, fri).
+flight(sr805, london, greece, 1445, 1820, sat).
+flight(sr805, london, greece, 1445, 1820, sun).
+
+flight(ba613, greece, london, 0900, 1140, mon).
+flight(ba613, greece, london, 0900, 1140, tue).
+flight(ba613, greece, london, 0900, 1140, wed).
+flight(ba613, greece, london, 0900, 1140, thu).
+flight(ba613, greece, london, 0900, 1140, fri).
+flight(ba613, greece, london, 0900, 1140, sat).
+
+flight(sr806, greece, london, 1610, 1855, mon).
+flight(sr806, greece, london, 1610, 1855, tue).
+flight(sr806, greece, london, 1610, 1855, wed).
+flight(sr806, greece, london, 1610, 1855, thu).
+flight(sr806, greece, london, 1610, 1855, fri).
+flight(sr806, greece, london, 1610, 1855, sun).
+
+flight(ba510, london, paris, 0830, 1030, mon).
+flight(ba510, london, paris, 0830, 1030, tue).
+flight(ba510, london, paris, 0830, 1030, wed).
+flight(ba510, london, paris, 0830, 1030, thu).
+flight(ba510, london, paris, 0830, 1030, fri).
+flight(ba510, london, paris, 0830, 1030, sat).
+flight(ba510, london, paris, 0830, 1030, sun).
+
+flight(az459, london, paris, 1310, 1510, mon).
+flight(az459, london, paris, 1310, 1510, tue).
+flight(az459, london, paris, 1310, 1510, wed).
+flight(az459, london, paris, 1310, 1510, thu).
+flight(az459, london, paris, 1310, 1510, fri).
+flight(az459, london, paris, 1310, 1510, sat).
+flight(az459, london, paris, 1310, 1510, sun).
+
+flight(ba511, paris, london, 0910, 1020, mon).
+flight(ba511, paris, london, 0910, 1020, tue).
+flight(ba511, paris, london, 0910, 1020, wed).
+flight(ba511, paris, london, 0910, 1020, thu).
+flight(ba511, paris, london, 0910, 1020, fri).
+flight(ba511, paris, london, 0910, 1020, sat).
+flight(ba511, paris, london, 0910, 1020, sun).
+
+flight(az460, paris, london, 1220, 1330, mon).
+flight(az460, paris, london, 1220, 1330, tue).
+flight(az460, paris, london, 1220, 1330, wed).
+flight(az460, paris, london, 1220, 1330, thu).
+flight(az460, paris, london, 1220, 1330, fri).
+flight(az460, paris, london, 1220, 1330, sat).
+flight(az460, paris, london, 1220, 1330, sun).
+
+flight(jp322, paris, rome, 1130, 1240, tue).
+flight(jp322, paris, rome, 1130, 1240, wed).
+flight(jp322, paris, rome, 1130, 1240, thu).
+
+flight(jp323, rome, paris, 1130, 1240, tue).
+flight(jp323, rome, paris, 1130, 1240, wed).
+flight(jp323, rome, paris, 1130, 1240, thu).
+
+flight(fs619, rome, greece, 1440, 1630, mon).
+flight(fs619, rome, greece, 1440, 1630, wed).
+flight(fs619, rome, greece, 1440, 1630, thu).
+flight(fs619, rome, greece, 1440, 1630, fri).
+
+flight(fs620, greece, rome, 1100, 1310, mon).
+flight(fs620, greece, rome, 1100, 1310, wed).
+flight(fs620, greece, rome, 1100, 1310, thu).
+flight(fs620, greece, rome, 1100, 1310, fri).
 
 day(mon, 1).
 day(tue, 2).
@@ -45,6 +140,14 @@ day(thu, 4).
 day(fri, 5).
 day(sat, 6).
 day(sun, 7).
+
+location(1, singapore).
+location(2, london).
+location(3, edinburgh).
+location(4, greece).
+location(5, paris).
+location(6, rome).
+
 
 append(H, L, [H,L]).
 
@@ -62,16 +165,22 @@ time_difference_in_week(Start_time, Start_Day, End_time, End_day, Time_Diff):-
 	absolute_time(End_time, End_day, End_Abs), 
 	Time_Diff is (End_Abs - Start_Abs).
 
-adjusted_schedule(Start_time, End_time, Start_Day, End_day) :-
-	flight(First_Flight,X,Y,Start_time,End_time,Start_Day),
+find_flight(Number, Origin, Destination, Start_time, End_time, Start_Day, End_day):-
+	flight(Number, Origin, Destination, Start_time, End_time, Start_Day),
 	absolute_time(Start_time, Start_Day, Start_Abs), 
-	absolute_time(End_time, End_day, End_Abs), 
-	End_Abs < Start_Abs, day(Start_Day, Old_Day), New_Day_int is (Old_Day+1), day(New_Day, New_Day_int), End_day is New_Day;
-	End_day is Start_Day.
+	absolute_time(End_time, Start_Day, End_Abs), 
+	Diff is (End_Abs - Start_Abs),
+	fix_end_day(Diff, Start_Day, End_day).
+
+fix_end_day(Diff, Old_Day, New_Day):- Diff>=0, day(Old_Day, Old_Day_int), day(New_Day, Old_Day_int).
+fix_end_day(Diff, Old_Day, New_Day):- Diff<0, 
+	day(Old_Day, Old_Day_int),
+	New_Day_int is (Old_Day_int+1),
+	day(New_Day, New_Day_int).
 
 one_change_travel_time(First_Flight, FF_Day, Second_Flight, SF_Day, Travel_Time):- 
-	flight(First_Flight,X,Y,FF_Start,FF_End,FF_Day),
-	flight(Second_Flight,W,Z,SF_Start,SF_End,SF_Day),
+	find_flight(First_Flight,X,Y,FF_Start,FF_End,FF_Day,FF_End_day),
+	find_flight(Second_Flight,W,Z,SF_Start,SF_End,SF_Day,SF_End_day),
 	time_difference(FF_Start, FF_Day, FF_End, FF_Day, FF_Duration),
 	time_difference(SF_Start, SF_Day, SF_End, SF_Day, SF_Duration),
 	time_difference(FF_End, FF_Day, SF_Start, SF_Day, Transit_Time),
@@ -90,45 +199,45 @@ remove_duplicate([H|T],Out) :-
    remove_duplicate(T,Out).
 
 find_direct_flights(Origin, Destination, List):-
-	findall([X0, X1], flight(X0, Origin, Destination, Q, W, X1), X),
+	findall([X0, X1], find_flight(X0, Origin, Destination, Q, W, X1, E), X),
 	remove_duplicate(X, List).
 
 find_all_direct_flights_after(Origin, Destination, Time, Day, Number):-
-	flight(Number, Origin, Destination, Start_time, End_time, Flight_Day).
+	find_flight(Number, Origin, Destination, Start_time, End_time, Flight_Day, Flight_Arrival_Day).
 
 find_distinct_direct_flights_after(Origin, Destination, Time, Day, List):-
 	findall(X0, find_all_direct_flights_after(Origin, Destination, Time, Day, X0), X),
 	remove_duplicate(X, List).
 	
 find_all_flight_and_travel_time(Number, Origin, Destination, Time, Day, Travel_Time):-
-	flight(Number, Origin, Destination, Start_time, End_time, Flight_Day),
+	find_flight(Number, Origin, Destination, Start_time, End_time, Flight_Day, Flight_Arrival_Day),
 	time_difference(Time, Day, Start_time, Flight_Day, Travel_Time).
 
 list_flights_and_travel_times(Origin, Destination, Time, Day, List):-
 	findall([Number, Travel_Time], find_all_flight_and_travel_time(Number, Origin, Destination, Time, Day, Travel_Time), X).
 
 find_direct_flight(Origin, Destination, Day):-
-	flight(Number, Origin, Destination, _, _, Day),
+	find_flight(Number, Origin, Destination, _, _, Day, Flight_Arrival_Day),
 	write(Origin), write(' to '), write(Destination), write('\n').
 
 
 
 find_flights(Origin, Destination, Number):-
 	find_direct_flight(Origin, Destination, mon);
-	flight(W, Origin, Buffer, X, Y, Z),
+	find_flight(W, Origin, Buffer, X, Y, Z, Flight_Arrival_Day),
 	find_flights(Buffer, Destination, Number).
 
 
 
 %% CASE 1: Next node is the destination
 route(Origin, Destination, DeptTime, ArrTime, Day, [[Flightno, New_Day]], Visited):-
-    flight(Flightno, Origin, Destination, Dept1, Arr1, New_Day),
+    find_flight(Flightno, Origin, Destination, Dept1, Arr1, New_Day, Flight_Arrival_Day),
     not(member(Destination,Visited)).
         
 
 %% CASE 2: 
 route(Origin, Destination, Dept_Time, Arr_Time, Day, [[Flightno, New_Day]|Route], Visited):-
-    flight(Flightno, Origin, Transit, DeptTime, ArrTime, New_Day),
+    find_flight(Flightno, Origin, Transit, DeptTime, ArrTime, New_Day, Flight_Arrival_Day),
     not(member(Transit,Visited)),
     route(Transit, Destination, Dept1, Arr1, Day, Route, [Transit|Visited]).
 	
@@ -138,14 +247,14 @@ plan_route(Origin_City, Destination_City, Day, Route):-
     route(Origin_City, Destination_City, Dept_Time, Arr_Time, Day, Route, [Origin_City]).
 
 
-process_flight_node([Number, Day|Empty], Start_time, End_time, Day):-
-	flight(Number, Origin, Destination, Start_time, End_time, Day).
+process_flight_node([Number, Day|Empty], Start_time, End_time, Day, Flight_Arrival_Day):-
+	find_flight(Number, Origin, Destination, Start_time, End_time, Day, Flight_Arrival_Day).
 
 calculate_route_time([First_Flight|Route], Time):-	
 	process_route([First_Flight|Route], 0, Time).
 
 calculate_route_time_from_specified_time([First_Flight|Route], Start_time, Start_Day, Time):-	
-	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day),
+	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day, FF_Arrival_day),
 	time_difference(Start_time, Start_Day, FF_Start, FF_Day, InitialWait),
 	process_route([First_Flight|Route], 0, Route_time),
 	Time is (Route_time + InitialWait).
@@ -180,25 +289,25 @@ extract_all_times(Origin, Destination, Day, Route, List):-
 	extract_times(RouteList,[], List).
 
 process_route([First_Flight, Second_Flight|Route], Time, Result):-
-	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day),
-	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day),
-	time_difference(FF_Start, FF_Day, FF_End, FF_Day, FF_Duration),
-	time_difference(FF_End, FF_Day, SF_Start, SF_Day, Transit_Time),
+	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day, FF_Arrival_day),
+	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day, SF_Arrival_day),
+	time_difference(FF_Start, FF_Day, FF_End, FF_Arrival_day, FF_Duration),
+	time_difference(FF_End, FF_Arrival_day, SF_Start, SF_Day, Transit_Time),
 	New_time is (Time + FF_Duration + Transit_Time),
 	process_route([Second_Flight|Route], New_time, Result).
 
 process_route([First_Flight, Second_Flight|[]], Time, Result):-
-	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day),
-	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day),
-	time_difference(FF_Start, FF_Day, FF_End, FF_Day, FF_Duration),
-	time_difference(SF_Start, SF_Day, SF_End, SF_Day, SF_Duration),
-	time_difference(FF_End, FF_Day, SF_Start, SF_Day, Transit_Time),
+	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day, FF_Arrival_day),
+	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day, SF_Arrival_day),
+	time_difference(FF_Start, FF_Day, FF_End, FF_Arrival_day, FF_Duration),
+	time_difference(SF_Start, SF_Day, SF_End, SF_Arrival_day, SF_Duration),
+	time_difference(FF_End, FF_Arrival_day, SF_Start, SF_Day, Transit_Time),
 	New_time is (Time + FF_Duration + Transit_Time + SF_Duration),
 	process_route([], New_time, Result).
 
 process_route([First_Flight|[]], Time, Result):-
-	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day),
-	time_difference(FF_Start, FF_Day, FF_End, FF_Day, FF_Duration),
+	process_flight_node(First_Flight, FF_Start, FF_End, FF_Day, FF_Arrival_day),
+	time_difference(FF_Start, FF_Day, FF_End, FF_Arrival_day, FF_Duration),
 	Result is (Time + FF_Duration).
 	
 min_in_list([Min],Min).
@@ -231,7 +340,7 @@ extract_fastest_route_from_list([], MinTime):-false.
 	
 
 print_route([[Number, Day|Empty], Second_Flight|Route]):-
-	flight(Number, Origin, Destination, Start_time, End_time, Day),
+	find_flight(Number, Origin, Destination, Start_time, End_time, Day, Flight_Arrival_Day),
 	write('     Flight Number: '), 
 	write(Number), 
 	write(' ('),
@@ -246,13 +355,15 @@ print_route([[Number, Day|Empty], Second_Flight|Route]):-
 	write('\n'),
 	write('		Arrival  :'),
 	print_time(End_time),
+	write(' on '),
+	write(Flight_Arrival_Day),
 	write('.\n'),
-	time_difference(Start_time, Day, End_time, Day, Duration),
+	time_difference(Start_time, Day, End_time, Flight_Arrival_Day, Duration),
 	write('		Duration :'),
 	print_time_difference(Duration),
 	write('\n'),
-	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day),
-	time_difference(End_time, Day, SF_Start, SF_Day, Transit_Time),
+	process_flight_node(Second_Flight, SF_Start, SF_End, SF_Day, SF_Arrival_day),
+	time_difference(End_time, Flight_Arrival_Day, SF_Start, SF_Day, Transit_Time),
 	write('\n'),
 	write('     Wating Time: '),
 	print_time_difference(Transit_Time),
@@ -263,7 +374,7 @@ print_route([[Number, Day|Empty], Second_Flight|Route]):-
 
 
 print_route([[Number, Day|Empty]|[]]):-
-	flight(Number, Origin, Destination, Start_time, End_time, Day),
+	find_flight(Number, Origin, Destination, Start_time, End_time, Day, Flight_Arrival_Day),
 	write('     Flight Number: '), 
 	write(Number), 
 	write(' ('),
@@ -278,8 +389,10 @@ print_route([[Number, Day|Empty]|[]]):-
 	write('\n'),
 	write('		Arrival  :'),
 	print_time(End_time),
+	write(' on '),
+	write(Flight_Arrival_Day),
 	write('.\n'),
-	time_difference(Start_time, Day, End_time, Day, Duration),
+	time_difference(Start_time, Day, End_time, Flight_Arrival_Day, Duration),
 	write('		Duration :'),
 	print_time_difference(Duration),
 	write('\n'),
@@ -309,6 +422,50 @@ print_minutes(Minutes):- Minutes<1, write('').
 
 
 
+main :- repeat, nl, write('---------------------------------------------'),
+        nl, write('	     Welcome to Flight Planner!'),
+		nl, write('	Let us Help you plan your travel!'),
+        nl, write('---------------------------------------------'),
+        nl, write('Please pick your Origin City'),
+        nl, write('1. Singapore'),
+        nl, write('2. London'),
+        nl, write('3. Edinburgh'),
+        nl, write('4. Greece'),
+        nl, write('5. Paris'),
+		nl, write('6. Rome'),nl,
+        read(Origin_int),
+        location(Origin_int, Origin),
+
+
+		nl,write('Please pick your Destination City'),
+        nl,write('1. Singapore'),
+        nl,write('2. London'),
+        nl,write('3. Edinburgh'),
+        nl,write('4. Greece'),
+        nl,write('5. Paris'),
+		nl,write('6. Rome'),nl,
+        read(Destination_int),
+        location(Destination_int, Destination),
+
+        nl,write('Please enter the day you want to travel on!'),
+        nl,write('1. Monday'),
+        nl,write('2. Tuesday'),
+        nl,write('3. Wednesday'),
+        nl,write('4. Thursday'),
+        nl,write('5. Friday'),
+		nl,write('6. Saturday'),
+        nl,write('7. Sunday'),nl,
+		read(Day_int),
+		day(Day, Day_int),
+		
+	 	nl,write('---------------------------------------------'),
+        nl,write('Searching for a Flight from '),
+		write(Origin), write(' to '),write(Destination),
+		nl,write('		 Please Wait..'),
+        nl,write('---------------------------------------------'),nl,
+		nl, find_fastest_route(Origin,Destination,Day).
+
+    
 
 
 
